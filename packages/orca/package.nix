20fe244ed1ded414ee8fb96fe7b090b51c037345
Jeremy Kennedy <me@jeremyk.net>
2026-09-08 19:32:58 -0400

    fix: clear ambient OMP environment in Orca launcher

--- packages/orca/package.nix
+++ packages/orca/package.nix
@@ -2,23 +2,37 @@
   appimageTools,
   fetchurl,
   lib,
+  makeWrapper,
   stdenv,
+  symlinkJoin,
 }:
 
 let
   versionData = builtins.fromJSON (builtins.readFile ./hashes.json);
   inherit (versionData) version hashes;
   platform = stdenv.hostPlatform.system;
+  base = appimageTools.wrapType2 {
+    pname = "orca";
+    inherit version;
+
+    src = fetchurl {
+      url = "https://github.com/stablyai/orca/releases/download/v${version}/orca-linux.AppImage";
+      hash = hashes.${platform};
+    };
+  };
 in
 assert platform == "x86_64-linux";
-appimageTools.wrapType2 {
-  pname = "orca";
-  inherit version;
+symlinkJoin {
+  name = "orca-${version}";
+  paths = [ base ];
+  nativeBuildInputs = [ makeWrapper ];
 
-  src = fetchurl {
-    url = "https://github.com/stablyai/orca/releases/download/v${version}/orca-linux.AppImage";
-    hash = hashes.${platform};
-  };
+  postBuild = ''
+    rm "$out/bin/orca"
+    makeWrapper ${base}/bin/orca "$out/bin/orca" \
+      --unset PI_CONFIG_FILES \
+      --unset PI_CODING_AGENT_DIR
+  '';
 
   meta = {
     description = "Agent development environment for parallel coding agents";
